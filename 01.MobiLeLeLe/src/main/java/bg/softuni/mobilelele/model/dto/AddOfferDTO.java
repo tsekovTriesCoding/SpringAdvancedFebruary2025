6b8975fe735ee48f0edf7dbab73dbf720b766e8b
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-01-10 16:20:36 +0200

    description error i18n

--- 01.MobiLeLeLe/src/main/java/bg/softuni/mobilelele/model/dto/AddOfferDTO.java
+++ 01.MobiLeLeLe/src/main/java/bg/softuni/mobilelele/model/dto/AddOfferDTO.java
@@ -5,24 +5,18 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
-public record AddOfferDTO(@NotNull(message = "{add.offer.description.length}")
-                          @Size(min = 5, max = 500, message = "{add.offer.description.length}")
-                          String description, @NotNull @PositiveOrZero Integer mileage,
-                          @NotNull @PositiveOrZero Integer price,
-                          @NotNull EngineTypeEnum engineType) {
-    public AddOfferDTO(@NotNull(message = "{add.offer.description.not.null}")
-                       @Size(message = "{add.offer.description.length}", min = 5, max = 500) String description,
-                       @NotNull @PositiveOrZero Integer mileage,
-                       @NotNull @PositiveOrZero Integer price,
-                       @NotNull EngineTypeEnum engineType) {
-        this.description = description;
-        this.mileage = mileage;
-        this.price = price;
-        this.engineType = engineType;
-    }
+public record AddOfferDTO(
+        @NotNull(message = "{add.offer.description.length}")
+        @Size(message = "{add.offer.description.length}",
+                min = 5,
+                max = 500) String description,//not necessarily from message source
+        @NotNull @PositiveOrZero Integer mileage,
+        @NotNull @PositiveOrZero Integer price,
+        @NotNull EngineTypeEnum engineType
+) {
 
     public static AddOfferDTO empty() {
-        return new AddOfferDTO((String) null, (Integer) null, (Integer) null, (EngineTypeEnum) null);
+        return new AddOfferDTO(null, null, null, null);
     }
 
     public @NotNull(
