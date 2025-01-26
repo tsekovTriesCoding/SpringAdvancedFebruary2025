3fe531162f4f649a1ae78b09b905ddddc8ed55ce
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-01-26 14:47:11 +0200

    for the 403 forbidden

--- 02.Pathfinder/src/main/java/bg/softuni/pathfinder/config/SecurityConfig.java
+++ 02.Pathfinder/src/main/java/bg/softuni/pathfinder/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,6 +15,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
